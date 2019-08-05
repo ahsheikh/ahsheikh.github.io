36326640574ca4ae58daa2a2efa1317379ceae1d
ahsheikh <hanangul12@yahoo.co.uk>
2019-08-05 18:36:08 +0500

    article added

--- Courses/NA/matlab/powermethod.m
+++ Courses/NA/matlab/powermethod.m
@@ -4,7 +4,7 @@ clear all
 clc
         
 
-A=[0 11 -5; -2 17 -7; -4 26 -10];              % provide matrix
+A=[1 -2 2; 1 1 -1; -2 -2 1];              % provide matrix
 v = ones(length(A),1);                  % Initial eigenvector, ones vector 
 iter = 10;                              % number of iterations
 tol = 10^-5; 
@@ -22,7 +22,6 @@ y=A*x
 dd=abs(norm(x)-n);
 n=norm(x)
 x=y/n
-pause
 end
 vec=x;
 value=n;
